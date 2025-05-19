58f4ef80dc37d0e15c2ff31bc6bd2a127084dbfa
Shahina <sonashekhu09@gmail.com>
2025-05-19 11:05:14 +0530

    first commit

---
+++ backend/config/db.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose'
+import 'dotenv/config'
+
+export const connectDB = async() => {
+    try {
+       await mongoose.connect(process.env.MONGODB_URI)
+       console.log("Connected to Database")
+    } catch (error) {
+        console.log(error)
+    }
+}
